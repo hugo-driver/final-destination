9363386aebf57790693d34a428b1071336df9d16
hugo-driver <hubgit@fastmail.de>
2025-08-30 10:47:37 +0300

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 export const url = process.env.URL || 'http://localhost:8080';
 // Extract domain from `url`
 export const domain = new URL(url).hostname;
-export const siteName = 'Ștefan Beldie';
+export const siteName = 'Beldie.ro';
 export const siteDescription = 'Eleventy starter for building modern, resilient websites';
 export const siteType = 'Person'; // schema
 export const locale = 'ro_RO';
